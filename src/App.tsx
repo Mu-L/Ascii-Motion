19721e5e3e54f613a243aa0274610129a1ac61b5
CameronFoxly <cameronfoxly@gmail.com>
2025-11-09 13:38:31 -0800

    Updated git ignore and mobile handlings

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { AuthProvider, useCloudProject, GalleryHeaderText, NotificationButton } 
 import { ThemeToggle } from './components/common/ThemeToggle'
 import { AccountButton } from './components/features/AccountButton'
 import { HamburgerMenu } from './components/features/HamburgerMenu'
+import { GalleryMobileMenu } from './components/features/GalleryMobileMenu'
 import { ExportImportButtons } from './components/features/ExportImportButtons'
 import { useCloudDialogState } from './hooks/useCloudDialogState'
 import { useCloudProjectActions } from './hooks/useCloudProjectActions'
@@ -24,7 +25,6 @@ import { PublishToGalleryDialogWrapper } from './components/features/PublishToGa
 import { PerformanceOverlay } from './components/common/PerformanceOverlay'
 import { EditorPage } from './pages/EditorPage'
 import { CommunityPage } from './pages/CommunityPage'
-import { Button } from './components/ui/button'
 
 /**
  * Inner component that uses auth hooks
@@ -205,31 +205,9 @@ function AppContent() {
               {isCommunityRoute ? (
                 /* Community Gallery Header Layout */
                 <>
-                  {/* Left side - CTA button */}
+                  {/* Left side - Hamburger menu */}
                   <div className="flex gap-3 items-center flex-shrink-0">
-                    {/* "Start creating" CTA button with arrow */}
-                    <Button
-                      onClick={() => {
-                        window.location.href = 'https://ascii-motion.app';
-                      }}
-                      size="default"
-                      className="hidden sm:flex gap-2"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M19 12H5M12 19l-7-7 7-7" />
-                      </svg>
-                      Start creating
-                    </Button>
+                    <GalleryMobileMenu />
                   </div>
                   
                   {/* Center - Animated Gallery Text (slides in from bottom) - Hidden below 1210px */}
